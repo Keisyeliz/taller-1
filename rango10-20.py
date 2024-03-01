3d3aea342de88e9123811bb7d2d37a5fb6455ed6
Keisyeliz <158522128+Keisyeliz@users.noreply.github.com>
2024-03-01 11:36:21 -0500

    Update rango10-20.py

--- rango10-20.py
+++ rango10-20.py
@@ -1,10 +1,10 @@
 var_numInt = int(input('ingrese la cantidad de números a evaluar -> '))
-contador = 0
-
-for i in range(var_numInt):
-    num = float(input('Ingrese el número -> '))
-    if num >= 10 and num <= 20:
-        contador += 1
-        
-print('la cantidad de número dentro del rango es:',contador)
+contadorInt = 1
+contadornumInt = 0
+while contadorInt <= var_numInt:
+    numeroInt = int(input('ingrese un número -> '))
+    if numeroInt >= 10 and numeroInt <= 20:
+        contadornumInt += 1
+    contadorInt += 1
+print('La cantidad de números dentro de rango de 10-20 son: ', contadornumInt)
     
