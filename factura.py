da9c290ef214735d96d146b7e4e7d9f7380358f0
Keisyeliz <158522128+Keisyeliz@users.noreply.github.com>
2024-03-01 13:17:23 -0500

    Update factura.py

--- factura.py
+++ factura.py
@@ -1,18 +1,22 @@
-
 import os
 os.system('cls')
 var_totalGFlt =0
-cons_computadormesaInt = 1500000
 cons_computadorescritorioInt = 1000000 
+cons_computadorportatilInt = 1500000
 cons_tabletasInt = 500000
 cons_videobeamInt = 400000
 cons_tvInt = 800000
 
-cant_computadormesa = 0
+cant_computadorportatil = 0
+total_computadorportatil = 0
 cant_computadorescritorio = 0
+total_computadorescritorio = 0
 cant_tabletas = 0
+total_tabletas = 0
 cant_videobeam = 0
+total_videobeam = 0
 cant_tv = 0
+total_tv = 0
 
 
 var_nombreStr = input('Nombre del cliente -> ') 
@@ -22,42 +26,51 @@ var_controlBln = True
 while var_controlBln ==True:
     os.system('cls')
     print('cliente:  ',var_nombreStr)
-    var_opcionStr = int(input('---MENÚ DE OPCIONES---\n\n1. computador de mesa\n2. computador de escritorio\n3. Tabletas\n4. videobeam\n5. tv\n6. Facturar\n -> '))
-    if var_opcionStr >= 1 and var_opcionStr <=5:
+    var_opcionStr = int(input('---MENÚ DE OPCIONES---\n\n1. computador portatil\n2. computador de escritorio\n3. Tabletas\n4. videobeam\n5. tv\n6. Facturar\n -> '))
+    if var_opcionStr >= 1 and var_opcionStr <= 5:
         var_cantidadInt = int(input('Ingrese la cantidad->'))
         
     if var_opcionStr == 1:
-        var_totalGFlt += (var_cantidadInt * cons_computadormesaInt)
-        cant_computadormesa = var_cantidadInt
+        var_totalGFlt += (var_cantidadInt * cons_computadorportatilInt)
+        cant_computadorportatil = var_cantidadInt
+        total_computadorportatil = (var_cantidadInt * cons_computadorportatilInt)
+
         
     if var_opcionStr == 2:
         var_totalGFlt += (var_cantidadInt * cons_computadorescritorioInt)
         cant_computadorescritorio = var_cantidadInt
+        total_computadorescritorio = (var_cantidadInt * cons_computadorescritorioInt)
     
     if var_opcionStr == 3:
         var_totalGFlt += (var_cantidadInt * cons_tabletasInt)
         cant_tabletas = var_cantidadInt
+        total_tabletas = (var_cantidadInt * cons_tabletasInt)
 
     if var_opcionStr == 3:
         var_totalGFlt += (var_cantidadInt * cons_videobeamInt)
         cant_videobeam = var_cantidadInt
+        total_videobeam = (var_cantidadInt * cons_videobeamInt)
 
     if var_opcionStr == 4:
         var_totalGFlt += (var_cantidadInt * cons_tvInt)
         cant_tv = var_cantidadInt
+        total_tv = (var_cantidadInt * cons_tvInt)
         
     if var_opcionStr == 6:
             print('<<<<FACTURA>>>>')
-            print('Nombre del cliente',var_nombreStr)
-            print('Contacto del cliente',var_contactoStr)
-            print('Direccion del cliente',var_direccionStr)
-            print("Computadores de mesa:", cant_computadormesa)
-            print("Computadores de escritorio:", cant_computadorescritorio)
-            print("Tabletas:", cant_tabletas)
-            print("Videobeams:", cant_videobeam)
-            print("Televisores:", cant_tv)
+            print('Nombre del cliente: ',var_nombreStr)
+            print('Contacto del cliente: ',var_contactoStr)
+            print('Direccion del cliente: ',var_direccionStr)
+            print("Computadores portatiles: ", cant_computadorportatil,'$',total_computadorportatil)
+            print("Computadores de escritorio: ", cant_computadorescritorio,'$',total_computadorescritorio)
+            print("Tabletas: ", cant_tabletas,'$',total_tabletas)
+            print("Videobeams: ", cant_videobeam,'$',total_videobeam)
+            print("Televisores: ", cant_tv, '$',total_tv)
 
             print('El total a pagar es $',var_totalGFlt)
             print('Gracias por su compra')
             var_controlBln = False
-    
+
+
+
+   
