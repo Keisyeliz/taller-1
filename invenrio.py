acff7807de9c9ca62c4c9e044f825141dcd6bc81
Keisyeliz <158522128+Keisyeliz@users.noreply.github.com>
2024-03-01 13:36:14 -0500

    Update invenrio.py

--- invenrio.py
+++ invenrio.py
@@ -1,5 +1,4 @@
 
-
 codigoInt = int(input('Código-> '))
 nombreStr = input('Nombre -> ')
 existenciasInt = 0
@@ -8,22 +7,26 @@ import os
 while controlBln == True:
     os.system('cls')
     print('codigo: ',codigoInt,' \nNombre: ',nombreStr,'\nExistencias: ',existenciasInt)
-    opcionStr = input('1. Compras\n2. Ventas\n3. Reportes\n4. Salir\n -> ')
-    cantidadInt = int(input('Cantidad-> '))
+    var_opcionStr = int(input('1. Compras\n2. Ventas\n3. Reportes\n4. Salir\n -> '))
+    if var_opcionStr >= 1 and var_opcionStr <= 2:
+        cantidadInt = int(input('Ingrese la cantidad->'))
     
-    if opcionStr == '1':
+    
+    if var_opcionStr == 1:
         existenciasInt += cantidadInt
         
-    if opcionStr == '2':
+    if var_opcionStr == 2:
         existenciasInt -= cantidadInt
         if cantidadInt <= existenciasInt:
            existenciasInt -= cantidadInt
         else:
             print ('No hay suficientes existencias')
             
-    if opcionStr == '3':
+    if var_opcionStr == 3:
         print('Cantidad de existencias actuales del inventario',existenciasInt)
         enter = input ('<ENTER>')
         
-    if opcionStr == '4':
+    if var_opcionStr == 4:
         controlBln = False
+
+         
